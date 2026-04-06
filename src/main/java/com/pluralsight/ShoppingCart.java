9ab88e0c0359a8c705ea48e644fb6d462ac58c50
WendolynQuartey <quarteywendolyn@gmail.com>
2026-04-06 11:04:06 -0400

    Created Grocery List

---
+++ src/main/java/com/pluralsight/ShoppingCart.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+public class ShoppingCart {
+    public static void main(String[] args){
+        System.out.println("My Shopping List");
+        System.out.println("Pineapple");
+        System.out.println("Salmon Fillet");
+        System.out.println("Granola");
+        System.out.println("Greek Yogurt");
+        System.out.println("Ground Beef");
+        System.out.println("Pop Corners");
+        System.out.println("White Rice");
+        System.out.println("Seaweed Snacks");
+        System.out.println("Elbow Macaroni");
+        System.out.println("Garlic & Herb Sauce");
+    }
+}
